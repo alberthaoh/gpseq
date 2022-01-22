fc2e37753b17bfac6a5101abbc0a6002cfdd27fe
Hao Huang <hao.huang@tele2.com>
2022-01-22 14:10:03 +0100

    initial commit

---
+++ docker/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir -p ./src/
+cp ../src/*.py ./src/
+cp ../requirements.txt .
+image=gpseq
+version=1.0
+registry=bicrolab
+docker image rm $image
+docker image rm $registry/$image:$version
+docker image rm $registry/$image:latest
+docker build -t $image -f ./Dockerfile .
+docker tag $image $registry/$image:$version
+docker tag $image $registry/$image:latest
+
+#docker push $registry
+# docker push $registry/$image:latest
+# docker push $registry/$image:$version
